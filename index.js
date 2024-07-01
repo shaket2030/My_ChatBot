66312129dc6f62f7e50432a1b6254137e1547606
saket <saketjh123@gmail.com>
2024-07-01 08:32:38 +0530

    first change

---
+++ index.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI('AIzaSyBtAtXqubO_do8UZvJug1tqRyXNM6SWNfc');
+
+// ...
+
+// The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+// ...
+async function run() {
+    const prompt = "Write a story about a magic backpack."
+  
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+  
+  run();
